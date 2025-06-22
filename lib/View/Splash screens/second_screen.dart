de9655ae05d9fcf8b0363c01f7a307ba5e9222e0
Md Rifat Alam <mdrftlm@gmail.com>
2025-06-22 22:25:59 +0600

    second screen

--- lib/View/Splash screens/second_screen.dart
+++ lib/View/Splash screens/second_screen.dart
@@ -16,16 +16,28 @@ class _SecondScreenState extends State<SecondScreen> {
         child: Column(
           children: [
             const SizedBox(
-              height: 100,
+              height: 150,
             ),
             Container(
-              height: 200,
-              width: 200,
+              height: 150,
+              width: 150,
               decoration: const BoxDecoration(
                   image: DecorationImage(
                       image: AssetImage(
                           "assets/image/Pic_23-09-23_22-59-11-673 1.png"))),
-            )
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            const Text(
+              "Are you Engineer or ",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "Customer?",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+
           ],
         ),
       ),
