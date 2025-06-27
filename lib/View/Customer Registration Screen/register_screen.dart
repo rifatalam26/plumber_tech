d2fcdaec2866f8dd8eef60c7fd4bcd6bf57f3d24
Md Rifat Alam <mdrftlm@gmail.com>
2025-06-27 21:53:11 +0600

    register screen

--- lib/View/Customer Registration Screen/register_screen.dart
+++ lib/View/Customer Registration Screen/register_screen.dart
@@ -8,6 +8,7 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  bool _secureText = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,8 +29,243 @@ class _RegisterScreenState extends State<RegisterScreen> {
           ),
           const Text(
             "Register",
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          )
+            style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff192A48)),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              const SizedBox(
+                width: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "First Name",
+                    style: TextStyle(fontSize: 14, color: Color(0xff192A48)),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            width: 1.5, color: const Color(0xff798090)),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const Row(
+                      children: [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text("John"),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "First Name",
+                    style: TextStyle(fontSize: 14, color: Color(0xff192A48)),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            width: 1.5, color: const Color(0xff798090)),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const Row(
+                      children: [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text("Doe"),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              SizedBox(child: Text("E-mail")),
+            ],
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Enter your email",
+                hintStyle: TextStyle(color: Color(0xff798090)),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(color: Color(0xff798090))),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(color: Color(0xff798090))),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              SizedBox(child: Text("Password")),
+            ],
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: TextField(
+              obscureText: _secureText,
+              decoration: InputDecoration(
+                  hintText: "*******",
+                  hintStyle: const TextStyle(color: Color(0xff798090)),
+                  enabledBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide(color: Color(0xff798090))),
+                  focusedBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide(color: Color(0xff798090))),
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _secureText = !_secureText;
+                        });
+                      },
+                      icon: Icon(
+                        _secureText ? Icons.visibility_off : Icons.visibility,
+                        color: Colors.grey.shade400,
+                      ))),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              Text("The password should contain upper case,",
+                  style: TextStyle(fontSize: 13, color: Color(0xff798090))),
+            ],
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                "special character & 8 character",
+                style: TextStyle(fontSize: 13, color: Color(0xff798090)),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 13,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              SizedBox(child: Text("Confirm Password")),
+            ],
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: TextField(
+              obscureText: _secureText,
+              decoration: InputDecoration(
+                  hintText: "*******",
+                  hintStyle: const TextStyle(color: Color(0xff798090)),
+                  enabledBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide(color: Color(0xff798090))),
+                  focusedBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide(color: Color(0xff798090))),
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _secureText = !_secureText;
+                        });
+                      },
+                      icon: Icon(
+                        _secureText ? Icons.visibility_off : Icons.visibility,
+                        color: Colors.grey.shade400,
+                      ))),
+            ),
+          ),
+          const SizedBox(
+            height: 13,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              SizedBox(child: Text("Address")),
+            ],
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: TextField(
+              obscureText: _secureText,
+              decoration: const InputDecoration(
+                  hintText: "Enter your password",
+                  hintStyle: TextStyle(color: Color(0xff798090)),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide(color: Color(0xff798090))),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide(color: Color(0xff798090))),
+                  suffixIcon: Icon(
+                    Icons.location_on,
+                    color: Color(0xff3B8CDB),
+                  )),
+            ),
+          ),
         ],
       ),
     );
