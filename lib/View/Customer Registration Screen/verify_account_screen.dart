04db4307fcf6f29a9f82942a2c6ece4a0b312f3d
Md Rifat Alam <mdrftlm@gmail.com>
2025-06-29 21:25:02 +0600

    Verify Account Screen

--- lib/View/Customer Registration Screen/verify_account_screen.dart
+++ lib/View/Customer Registration Screen/verify_account_screen.dart
@@ -34,16 +34,66 @@ class _VerifyAccountScreenState extends State<VerifyAccountScreen> {
                 style: TextStyle(fontSize: 14, color: Color(0xff2D3444)),
               ),
               SizedBox(
-                height: 50,
+                height: 30,
               ),
               Row(
                 children: [
+                  SizedBox(
+                    width: 20,
+                  ),
                   Text(
                     "Enter Code",
                     style: TextStyle(fontSize: 14, color: Color(0xff192A48)),
                   ),
                 ],
-              )
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "4 Digit Code",
+                    hintStyle:
+                        TextStyle(color: Color(0xff7F756B), fontSize: 14),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(color: Color(0xff798090))),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(color: Color(0xff798090))),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 75,
+                  ),
+                  Text(
+                    "Did’t Receive Code?",
+                    style: TextStyle(fontSize: 14, color: Color(0xff192A48)),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "Resend Code",
+                    style: TextStyle(fontSize: 14, color: Color(0xff3B8CDB)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Resend code in 00:59",
+                style: TextStyle(fontSize: 14, color: Color(0xff192A48)),
+              ),
             ],
           ),
         ),
