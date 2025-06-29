ef98609160bac7a98c90e1558292a9bef8be6076
Md Rifat Alam <mdrftlm@gmail.com>
2025-06-29 20:57:30 +0600

    Verify Account Screen

---
+++ lib/View/Customer Registration Screen/vreify_account_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class VreifyAccountScreen extends StatefulWidget {
+  const VreifyAccountScreen({super.key});
+
+  @override
+  State<VreifyAccountScreen> createState() => _VreifyAccountScreenState();
+}
+
+class _VreifyAccountScreenState extends State<VreifyAccountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
