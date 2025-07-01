a4450410b81609520c9c229b0dcba21bf3d69b27
Md Rifat Alam <mdrftlm@gmail.com>
2025-07-01 20:53:59 +0600

    forgot_password_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plumber_tech/View/Customer%20Registration%20Screen/forgot_password_screen.dart';
 import 'package:plumber_tech/View/Customer%20Registration%20Screen/verify_account_screen.dart';
 
 void main(){
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:VerifyAccountScreen() ,
+      home:ForgotPasswordScreen() ,
     );
   }
 }
