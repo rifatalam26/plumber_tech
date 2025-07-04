32947e6aeb1cc3c3a1eefd060d019a84ff88e1f9
Md Rifat Alam <mdrftlm@gmail.com>
2025-07-04 20:21:31 +0600

    Home page

--- lib/View/Home screens/home_page.dart
+++ lib/View/Home screens/home_page.dart
@@ -184,8 +184,44 @@ class _HomePageState extends State<HomePage> {
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(15),
                       image: const DecorationImage(
+                          image: AssetImage("assets/image/image 2200.png"))),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  height: 100,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: const DecorationImage(
                           image: AssetImage("assets/image/image 2199.png"))),
                 ),
+              ],
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "Boiler Fault",
+                  style: TextStyle(fontSize: 14, color: Color(0xff192A48)),
+                ),
+                SizedBox(
+                  width: 75,
+                ),
+                Text(
+                  "Boiler Pressure",
+                  style: TextStyle(fontSize: 14, color: Color(0xff192A48)),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
                 const SizedBox(
                   width: 20,
                 ),
@@ -197,7 +233,86 @@ class _HomePageState extends State<HomePage> {
                       image: const DecorationImage(
                           image: AssetImage("assets/image/img = 01 (1).png"))),
                 ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  height: 100,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: const DecorationImage(
+                          image: AssetImage("assets/image/img = 01.png"))),
+                ),
               ],
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "Leak",
+                  style: TextStyle(fontSize: 14, color: Color(0xff192A48)),
+                ),
+                SizedBox(
+                  width: 75,
+                ),
+                Text(
+                  "Other",
+                  style: TextStyle(fontSize: 14, color: Color(0xff192A48)),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  height: 100,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: const DecorationImage(
+                          image: AssetImage("assets/image/image 2200.png"))),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  height: 100,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: const DecorationImage(
+                          image: AssetImage("assets/image/img = 01.png"))),
+                ),
+              ],
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "Boiler Fault",
+                  style: TextStyle(fontSize: 14, color: Color(0xff192A48)),
+                ),
+                SizedBox(
+                  width: 75,
+                ),
+                Text(
+                  "Other",
+                  style: TextStyle(fontSize: 14, color: Color(0xff192A48)),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 50,
             ),
           ],
         ),
