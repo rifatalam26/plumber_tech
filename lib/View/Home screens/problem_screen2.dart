866717c2177626251272294259de9ce283cf933f
Md Rifat Alam <mdrftlm@gmail.com>
2025-07-15 23:07:53 +0600

    problem_screen2.dart

--- lib/View/Home screens/problem_screen2.dart
+++ lib/View/Home screens/problem_screen2.dart
@@ -13,7 +13,65 @@ class _ProblemScreen2State extends State<ProblemScreen2> {
     return Scaffold(
       body: Column(
         children: [
-
+          const SizedBox(
+            height: 50,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 15,
+              ),
+              Icon(
+                Icons.arrow_back,
+                size: 25,
+                color: Color(0xff192A48),
+              ),
+              SizedBox(
+                width: 70,
+              ),
+              Text(
+                "Engineer level",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff192A48)),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                "Please,Add Your Problem Picture",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff192A48)),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                "Description",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff192A48)),
+              )
+            ],
+          ),
         ],
       ),
     );
