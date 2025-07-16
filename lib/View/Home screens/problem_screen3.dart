5125963faac6db37e098c9b6fd56bf6a21f57647
Md Rifat Alam <mdrftlm@gmail.com>
2025-07-16 23:24:21 +0600

    problem_screen3.dart

---
+++ lib/View/Home screens/problem_screen3.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ProblemScreen3 extends StatefulWidget {
+  const ProblemScreen3({super.key});
+
+  @override
+  State<ProblemScreen3> createState() => _ProblemScreen3State();
+}
+
+class _ProblemScreen3State extends State<ProblemScreen3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          CircularProgressIndicator()
+        ],
+      ),
+    );
+  }
+}
