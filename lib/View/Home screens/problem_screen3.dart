f0ca13aa4a3391816faecbd3ac6a86fbdedc2ee9
Md Rifat Alam <mdrftlm@gmail.com>
2025-07-17 23:08:53 +0600

    problem_screen3.dart

--- lib/View/Home screens/problem_screen3.dart
+++ lib/View/Home screens/problem_screen3.dart
@@ -26,7 +26,7 @@ class _ProblemScreen3State extends State<ProblemScreen3> {
               style: TextStyle(color: Color(0xff192A48), fontSize: 17),
             ),
             SizedBox(
-              height: 200,
+              height: 230,
             ),
             Column(
               children: [
