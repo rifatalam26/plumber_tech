549397d60a78fe8c5aa3f344295c83b24f6e2808
Md Rifat Alam <mdrftlm@gmail.com>
2025-07-18 21:32:14 +0600

    problem_screen3.dart

--- lib/View/Home screens/question_screen.dart
+++ lib/View/Home screens/question_screen.dart
@@ -43,11 +43,29 @@ class _QuestionScreenState extends State<QuestionScreen> {
               ),
               Text(
                 "What is the current pressure level on your boiler?",
-                style: TextStyle(
-                     color: Color(0xff000000)),
+                style: TextStyle(color: Color(0xff000000)),
               ),
             ],
-          )
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 48,
+              ),
+              Text(
+                ".",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "Options: Low, Normal, High, Unknown",
+                style: TextStyle(color: Color(0xff000000)),
+              ),
+            ],
+          ),
         ],
       ),
     );
