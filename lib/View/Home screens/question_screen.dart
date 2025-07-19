7c1f5e0f581568b96b3f7a2d463217b739dc3a12
Md Rifat Alam <mdrftlm@gmail.com>
2025-07-19 23:10:05 +0600

    problem_screen3.dart

--- lib/View/Home screens/question_screen.dart
+++ lib/View/Home screens/question_screen.dart
@@ -192,6 +192,208 @@ class _QuestionScreenState extends State<QuestionScreen> {
               )
             ],
           ),
+          Row(
+            children: [
+              SizedBox(
+                width: 26,
+              ),
+              Text(
+                ".",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "What type of leak do you have?",
+                style: TextStyle(color: Color(0xff000000)),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 26,
+              ),
+              Text(
+                ".",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "What is the current pressure level your boiler?",
+                style: TextStyle(color: Color(0xff000000)),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 48,
+              ),
+              Text(
+                ".",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "Options: Low, Normal, High, Unknown",
+                style: TextStyle(color: Color(0xff000000)),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 26,
+              ),
+              Text(
+                ".",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "Is the completely or showing signs of a problem?",
+                style: TextStyle(color: Color(0xff000000)),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 48,
+              ),
+              Text(
+                ".",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "Options: Functional, Showing of a problem",
+                style: TextStyle(color: Color(0xff000000)),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 26,
+              ),
+              Text(
+                ".",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "When was the boiler last serviced?",
+                style: TextStyle(color: Color(0xff000000)),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 48,
+              ),
+              Text(
+                ".",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "Options: Within the last year, 1-2 a years Never",
+                style: TextStyle(color: Color(0xff000000)),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 26,
+              ),
+              Text(
+                ".",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "Do you notice any warning codes on the boiler?",
+                style: TextStyle(color: Color(0xff000000)),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 48,
+              ),
+              Text(
+                ".",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "Options: Yes, No",
+                style: TextStyle(color: Color(0xff000000)),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 18,
+              ),
+              Text(
+                "3. Boiler Fault",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 48,
+              ),
+              Text(
+                ".",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Color(0xff000000)),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                "Options: Yes, No",
+                style: TextStyle(color: Color(0xff000000)),
+              ),
+            ],
+          ),
         ],
       ),
     );
