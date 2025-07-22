2ae7379c27ed0cfe2abf52b71d2ef88a4494fead
Md Rifat Alam <mdrftlm@gmail.com>
2025-07-22 23:20:15 +0600

    problem_screen.dart

--- lib/View/Experts Engineer screens/task_status_screen.dart
+++ lib/View/Experts Engineer screens/task_status_screen.dart
@@ -10,52 +10,65 @@ class TaskStatusScreen extends StatefulWidget {
 class _TaskStatusScreenState extends State<TaskStatusScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 50,
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 15,
+    return  Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(
+                  Icons.arrow_back,
+                  size: 25,
+                  color: Color(0xff192A48),
+                ),
+                SizedBox(
+                  width: 70,
+                ),
+                Text(
+                  "Boiler Pressure",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff192A48)),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 200,
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "Skills",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff192A48)),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 30,
+              width: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color:const Color(0xff798090),width: 1.5)
               ),
-              Icon(
-                Icons.arrow_back,
-                size: 25,
-                color: Color(0xff192A48),
-              ),
-              SizedBox(
-                width: 70,
-              ),
-              Text(
-                "Boiler Pressure",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xff192A48)),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 200,
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                "Skills",
-                style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xff192A48)),
-              )
-            ],
-          ),
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
