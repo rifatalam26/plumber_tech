68f466ffcfc2e71c2fe79355d2b67368c9efd809
Md Rifat Alam <mdrftlm@gmail.com>
2025-07-27 21:08:17 +0600

    Task Status Screen 0k

---
+++ lib/View/Payment Screen/payment_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class PaymentScreen extends StatefulWidget {
+  const PaymentScreen({super.key});
+
+  @override
+  State<PaymentScreen> createState() => _PaymentScreenState();
+}
+
+class _PaymentScreenState extends State<PaymentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(
+                  Icons.close,
+                  size: 25,
+                ),
+                Text(
+                  "Done",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff192A48)),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:plumber_tech/View/Customer%20Registration%20Screen/forgot_password_screen.dart';
-import 'package:plumber_tech/View/Customer%20Registration%20Screen/verify_account_screen.dart';
-import 'package:plumber_tech/View/Customer%20Registration%20Screen/verify_email_screen.dart';
-import 'package:plumber_tech/View/Home%20screens/home_page.dart';
-import 'package:plumber_tech/View/Home%20screens/problem_screen1.dart';
-import 'package:plumber_tech/View/Home%20screens/problem_screen2.dart';
-import 'package:plumber_tech/View/Home%20screens/problem_screen3.dart';
-import 'package:plumber_tech/View/Home%20screens/question_page_1.dart';
-import 'package:plumber_tech/View/Home%20screens/question_page_2.dart';
-import 'package:plumber_tech/View/Home%20screens/question_page_4.dart';
-import 'package:plumber_tech/View/Home%20screens/question_screen.dart';
+import 'View/Payment Screen/payment_screen.dart';
 
-import 'View/Customer Registration Screen/create_new_password_screen.dart';
-import 'View/Experts Engineer screens/problem_screen.dart';
-import 'View/Experts Engineer screens/task_status_screen.dart';
-
-void main(){
+void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -25,8 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:TaskStatusScreen() ,
+      home: PaymentScreen(),
     );
   }
 }
-
