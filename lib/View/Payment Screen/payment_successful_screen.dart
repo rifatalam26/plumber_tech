a39e1d8a01064ae5126e0903a3066d2a72ed212a
Md Rifat Alam <mdrftlm@gmail.com>
2025-08-01 09:58:47 +0600

    Payment Successful Screen

---
+++ lib/View/Payment Screen/payment_successful_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PaymentSuccessfulScreen extends StatefulWidget {
+  const PaymentSuccessfulScreen({super.key});
+
+  @override
+  State<PaymentSuccessfulScreen> createState() => _PaymentSuccessfulScreenState();
+}
+
+class _PaymentSuccessfulScreenState extends State<PaymentSuccessfulScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
