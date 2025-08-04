b8045ff6e8b4f9273412458e8cd99227d24652e3
Md Rifat Alam <mdrftlm@gmail.com>
2025-08-04 23:28:05 +0600

    Calling Screen

--- lib/View/Chat Screens/calling_screen.dart
+++ lib/View/Chat Screens/calling_screen.dart
@@ -11,25 +11,19 @@ class _CallingScreenState extends State<CallingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 500,
-          ),
-          // Container(
-          //   height: 80,
-          //   width: 80,
-          //   decoration: BoxDecoration(
-          //       borderRadius: BorderRadius.circular(100),
-          //       image: const DecorationImage(
-          //           image: AssetImage("assets/image/image 2181.png"))),
-          // )
-          CircleAvatar(
-            radius: 40,
-            //backgroundColor: Colors.blue,
-            backgroundImage: AssetImage("assets/image/image 2181.png"),
-          )
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 500,
+            ),
+            CircleAvatar(
+              radius: 40,
+              //backgroundColor: Colors.blue,
+              backgroundImage: AssetImage("assets/image/image 2181.png"),
+            )
+          ],
+        ),
       ),
     );
   }
