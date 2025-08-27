d959c4442b546de7e754dff5ad2125fe192f7794
Md Rifat Alam <mdrftlm@gmail.com>
2025-08-27 21:39:42 +0600

    video_calling_screen.dart

--- lib/View/Chat Screens/video_calling_screen.dart
+++ lib/View/Chat Screens/video_calling_screen.dart
@@ -14,14 +14,21 @@ class _VideoCallingScreenState extends State<VideoCallingScreen> {
       body: Container(
         decoration: const BoxDecoration(
             image: DecorationImage(
-              fit: BoxFit.cover,
+                fit: BoxFit.cover,
                 image: AssetImage("assets/image/image 2205.png"))),
         child: Column(
           children: [
             SizedBox(
               height: 200,
             ),
-            Center(child: Text("Milan Jack",style: TextStyle(),))
+            Center(
+                child: Text(
+              "Milan Jack",
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ))
           ],
         ),
       ),
