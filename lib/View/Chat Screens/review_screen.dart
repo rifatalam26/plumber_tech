a4158b80bb691289085daf5ad1f622edeec0f921
Md Rifat Alam <mdrftlm@gmail.com>
2025-08-29 10:14:17 +0600

    review_screen.dart

--- lib/View/Chat Screens/review_screen.dart
+++ lib/View/Chat Screens/review_screen.dart
@@ -25,7 +25,7 @@ class _ReviewScreenState extends State<ReviewScreen> {
                     size: 26,
                   )),
               const SizedBox(
-                width: 55,
+                width: 68,
               ),
               const Text(
                 "Task Review",
@@ -35,11 +35,40 @@ class _ReviewScreenState extends State<ReviewScreen> {
                     color: Color(0xff192A48)),
               ),
               const SizedBox(
-                width: 98,
+                width: 85,
               ),
               const CircleAvatar(
                 radius: 16,
                 backgroundImage: AssetImage("assets/image/Frame (11).png"),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Thank You For Using OK!",
+            style: TextStyle(fontSize: 14, color: Color(0xff798090)),
+          ),
+          const Text(
+            "Give a Review",
+            style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff192A48)),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 10,
+                width: 10,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                        image: AssetImage("assets/image/Star.png"))),
               )
             ],
           )
