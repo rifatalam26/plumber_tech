c39b57c80ce7cf08aa3ac250e61008f2a5afc2ca
Md Rifat Alam <mdrftlm@gmail.com>
2025-09-02 21:48:52 +0600

    review_screen.dart

--- lib/View/Chat Screens/review_screen.dart
+++ lib/View/Chat Screens/review_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class ReviewScreen extends StatefulWidget {
   const ReviewScreen({super.key});
@@ -11,41 +13,74 @@ class _ReviewScreenState extends State<ReviewScreen> {
   bool check = false;
   _showBottomSheet(BuildContext context) {
     showModalBottomSheet(
-        context: context,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
-        builder: (BuildContext context) {
-          return Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              children: [
-                Container(
-                  height: 50,
-                  width: 250,
-                  decoration: const BoxDecoration(
-                      color: Color(0xff034E9D),
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                  child: Row(
-                    children: [
-                      Checkbox(
-                          value: check,
-                          onChanged: (c) {
-                            check = c!;
-                            setState(() {});
-                          }),
-                      const Text(
-                        "Report an isu",
-                        style: TextStyle(fontSize: 14, color: Colors.white),
-                      )
-                    ],
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Log Out',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                "Are You Sure you Want to log out",
+                style: TextStyle(fontSize: 15, color: Color(0xff909090)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 30,
+                    width: 130,
+                    decoration: BoxDecoration(
+                        border: const Border(
+                            top: BorderSide(width: 2, color: Color(0xff7DBABB)),
+                            bottom:
+                            BorderSide(width: 2, color: Color(0xff7DBABB)),
+                            left:
+                            BorderSide(width: 2, color: Color(0xff7DBABB)),
+                            right:
+                            BorderSide(width: 2, color: Color(0xff7DBABB))),
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Center(
+                        child: Text(
+                          "Cancel",
+                          style: TextStyle(fontSize: 17, color: Color(0xff7DBABB)),
+                        )),
                   ),
-                )
-              ],
-            ),
-          );
-        });
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  Container(
+                    height: 30,
+                    width: 130,
+                    decoration: BoxDecoration(
+                        color: const Color(0xff7DBABB),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Center(
+                        child: Text(
+                          "Yes , log out",
+                          style: TextStyle(fontSize: 17, color: Colors.white),
+                        )),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,10 +112,7 @@ class _ReviewScreenState extends State<ReviewScreen> {
               ),
               InkWell(
                 onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => _showBottomSheet(context)));
+                  Get.to(_showBottomSheet(context));
                 },
                 child: const CircleAvatar(
                   radius: 16,
