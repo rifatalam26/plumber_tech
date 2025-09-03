f1fcfb3681ec3233bce8c7011f020b13d87b931b
Md Rifat Alam <mdrftlm@gmail.com>
2025-09-03 21:25:19 +0600

    review_screen.dart

--- lib/View/Chat Screens/review_screen.dart
+++ lib/View/Chat Screens/review_screen.dart
@@ -92,6 +92,7 @@ class _ReviewScreenState extends State<ReviewScreen> {
               Padding(
                 padding: const EdgeInsets.only(left: 8,right: 8),
                 child: TextFormField(
+                  maxLines: 3,
                   decoration: InputDecoration(
                     hintText: "Describe Issue",
                       hintStyle: const TextStyle(fontSize: 14,color: Color(0xff515868)),
@@ -116,179 +117,181 @@ class _ReviewScreenState extends State<ReviewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          Row(
-            children: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.close,
-                    size: 26,
-                  )),
-              const SizedBox(
-                width: 68,
-              ),
-              const Text(
-                "Task Review",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xff192A48)),
-              ),
-              const SizedBox(
-                width: 85,
-              ),
-              InkWell(
-                onTap: () {
-                  Get.to(_showBottomSheet(context));
-                },
-                child: const CircleAvatar(
-                  radius: 16,
-                  backgroundImage: AssetImage("assets/image/Frame (11).png"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.close,
+                      size: 26,
+                    )),
+                const SizedBox(
+                  width: 68,
                 ),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            "Thank You For Using OK!",
-            style: TextStyle(fontSize: 14, color: Color(0xff798090)),
-          ),
-          const Text(
-            "Give a Review",
-            style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Color(0xff192A48)),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Row(
-            children: [
-              const SizedBox(
-                width: 128,
-              ),
-              Container(
-                height: 15,
-                width: 15,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage("assets/image/Star.png"))),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                height: 15,
-                width: 15,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage("assets/image/Star icon.png"))),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                height: 15,
-                width: 15,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage("assets/image/Star icon (1).png"))),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                height: 15,
-                width: 15,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage("assets/image/Star icon (2).png"))),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                height: 15,
-                width: 15,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage("assets/image/Star icon (3).png"))),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Row(
-            children: [
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                "Detail Review",
-                style: TextStyle(fontSize: 14, color: Color(0xff444B5B)),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 120,
-            width: 320,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(color: const Color(0xff798090), width: 1.5)),
-            child: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 10,
+                const Text(
+                  "Task Review",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff192A48)),
+                ),
+                const SizedBox(
+                  width: 85,
+                ),
+                InkWell(
+                  onTap: () {
+                    Get.to(_showBottomSheet(context));
+                  },
+                  child: const CircleAvatar(
+                    radius: 16,
+                    backgroundImage: AssetImage("assets/image/Frame (11).png"),
                   ),
-                  Text(
-                    "Great job, my pressure problem has been",
-                    style: TextStyle(fontSize: 14, color: Color(0xff594F44)),
-                  ),
-                  Text(
-                    "resolved, I keep coming back to this company!",
-                    style: TextStyle(fontSize: 14, color: Color(0xff594F44)),
-                  )
-                ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Thank You For Using OK!",
+              style: TextStyle(fontSize: 14, color: Color(0xff798090)),
+            ),
+            const Text(
+              "Give a Review",
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff192A48)),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 128,
+                ),
+                Container(
+                  height: 15,
+                  width: 15,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/image/Star.png"))),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 15,
+                  width: 15,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/image/Star icon.png"))),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 15,
+                  width: 15,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/image/Star icon (1).png"))),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 15,
+                  width: 15,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/image/Star icon (2).png"))),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 15,
+                  width: 15,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/image/Star icon (3).png"))),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "Detail Review",
+                  style: TextStyle(fontSize: 14, color: Color(0xff444B5B)),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 120,
+              width: 320,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: const Color(0xff798090), width: 1.5)),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Great job, my pressure problem has been",
+                      style: TextStyle(fontSize: 14, color: Color(0xff594F44)),
+                    ),
+                    Text(
+                      "resolved, I keep coming back to this company!",
+                      style: TextStyle(fontSize: 14, color: Color(0xff594F44)),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 340,
-          ),
-          Container(
-            height: 50,
-            width: 330,
-            decoration: BoxDecoration(
-                color: const Color(0xff034E9D),
-                borderRadius: BorderRadius.circular(25)),
-            child: const Center(
-              child: Text(
-                "Submit",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
+            const SizedBox(
+              height: 340,
             ),
-          )
-        ],
+            Container(
+              height: 50,
+              width: 330,
+              decoration: BoxDecoration(
+                  color: const Color(0xff034E9D),
+                  borderRadius: BorderRadius.circular(25)),
+              child: const Center(
+                child: Text(
+                  "Submit",
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
