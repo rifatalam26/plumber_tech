9a5360215a8254b83edf41688883782a20ae3c55
Md Rifat Alam <mdrftlm@gmail.com>
2025-09-06 21:29:45 +0600

    profile_screen.dart

--- lib/View/Customer Profile/profile_screen.dart
+++ lib/View/Customer Profile/profile_screen.dart
@@ -13,7 +13,21 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       body: Column(
         children: [
-
+          Container(
+            width: 200,
+            color: const Color(0xff044D9B),
+            child: Column(
+              children: [
+                SizedBox(
+                  height:50 ,
+                ),
+                CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage("assets/image/Ellipse 3270 (1).png"),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:plumber_tech/View/Chat%20Screens/inbox_chat_screen_2.dart';
-import 'package:plumber_tech/View/Chat%20Screens/inbox_chat_screen_3.dart';
 
-import 'View/Chat Screens/inbox_chat_screen.dart';
-import 'View/Chat Screens/review_screen.dart';
-import 'View/Chat Screens/test1.dart';
-import 'View/Chat Screens/video_calling_screen.dart';
-import 'View/Home screens/question_page_1.dart';
+import 'package:plumber_tech/View/Customer%20Profile/profile_screen.dart';
+
+
 
 
 void main() {
@@ -20,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ReviewScreen(),
+      home:ProfileScreen(),
     );
   }
 }
