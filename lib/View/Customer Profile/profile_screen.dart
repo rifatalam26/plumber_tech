f63c7040ff9e8909345575fd51efc0a9edb7efd3
Md Rifat Alam <mdrftlm@gmail.com>
2025-09-06 21:35:17 +0600

    profile_screen.dart

--- lib/View/Customer Profile/profile_screen.dart
+++ lib/View/Customer Profile/profile_screen.dart
@@ -17,17 +17,32 @@ class _ProfileScreenState extends State<ProfileScreen> {
             width: 300,
             height: 500,
             color: const Color(0xff044D9B),
-            child: Column(
+            child: const Column(
               children: [
                 SizedBox(
-                  height:50 ,
+                  height: 50,
                 ),
                 Row(
                   children: [
                     CircleAvatar(
                       radius: 25,
-                      backgroundImage: AssetImage("assets/image/Ellipse 3270 (1).png"),
+                      backgroundImage:
+                          AssetImage("assets/image/Ellipse 3270 (1).png"),
                     ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Milan Jack",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        )
+                      ],
+                    )
                   ],
                 )
               ],
