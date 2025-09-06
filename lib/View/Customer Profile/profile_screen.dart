d0db646812eaaff026b0201a6c17a05ca45591e5
Md Rifat Alam <mdrftlm@gmail.com>
2025-09-06 22:23:29 +0600

    profile_screen.dart

--- lib/View/Customer Profile/profile_screen.dart
+++ lib/View/Customer Profile/profile_screen.dart
@@ -14,16 +14,19 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: Column(
         children: [
           Container(
-            width: 300,
+            width: 320,
             height: 500,
             color: const Color(0xff044D9B),
-            child: const Column(
+            child: Column(
               children: [
                 SizedBox(
                   height: 50,
                 ),
                 Row(
                   children: [
+                    SizedBox(
+                      width: 10,
+                    ),
                     CircleAvatar(
                       radius: 25,
                       backgroundImage:
@@ -33,6 +36,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       width: 10,
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           "Milan Jack",
@@ -40,8 +44,23 @@ class _ProfileScreenState extends State<ProfileScreen> {
                               fontSize: 16,
                               fontWeight: FontWeight.bold,
                               color: Colors.white),
-                        )
+                        ),
+                        Text(
+                          "example@gmail.com",
+                          style: TextStyle(fontSize: 14, color: Colors.white),
+                        ),
                       ],
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      height: 30,
+                      width: 30,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  "assets/image/edit profile option.png"))),
                     )
                   ],
                 )
