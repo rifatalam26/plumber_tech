117bbd7489774c4262bf3e409976f4deff0712ba
Md Rifat Alam <mdrftlm@gmail.com>
2025-09-06 22:42:25 +0600

    profile_screen.dart

--- lib/View/Customer Profile/profile_screen.dart
+++ lib/View/Customer Profile/profile_screen.dart
@@ -14,28 +14,28 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: Column(
         children: [
           Container(
-            width: 320,
+            width: 310,
             height: 500,
             color: const Color(0xff044D9B),
             child: Column(
               children: [
-                SizedBox(
-                  height: 50,
+                const SizedBox(
+                  height: 80,
                 ),
                 Row(
                   children: [
-                    SizedBox(
-                      width: 10,
+                    const SizedBox(
+                      width: 20,
                     ),
-                    CircleAvatar(
+                    const CircleAvatar(
                       radius: 25,
                       backgroundImage:
                           AssetImage("assets/image/Ellipse 3270 (1).png"),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       width: 10,
                     ),
-                    Column(
+                    const Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
@@ -52,15 +52,54 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       ],
                     ),
                     const SizedBox(
-                      width: 10,
+                      width: 35,
                     ),
                     Container(
-                      height: 30,
-                      width: 30,
+                      height: 38,
+                      width: 38,
                       decoration: const BoxDecoration(
                           image: DecorationImage(
                               image: AssetImage(
                                   "assets/image/edit profile option.png"))),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  height: 1,
+                  width: 250,
+                  color: Colors.white,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 25,
+                    ),
+                    Container(
+                      height: 40,
+                      width: 40,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("assets/image/icon.png"))),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    const Text(
+                      "Task History",
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                    const SizedBox(
+                      width: 75,
+                    ),
+                    const Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
                     )
                   ],
                 )
